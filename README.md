a9d740ad1039187124789ce9f04175613ddb08b7
seicham <onigirimentai422@gmail.com>
2023-04-07 12:29:46 +0900

    ER図自動生成

---
+++ README.md
@@ -0,0 +1,13 @@
+## tbls(ER図自動生成ツール)  
+https://github.com/k1LoW/tbls  
+  
+wslだとcurlでインストールできなかったので手動でインストールする必要があります。  
+  
+debファイルをカレントディレクトリにおいたら  
+```
+ dpkg -i tbls.deb
+```
+インストール先確認  
+```
+ dpkg -L tbls
+```
--- kyuyoCulculateService.sql
+++ kyuyoCulculateService.sql
@@ -3,11 +3,49 @@
 use kyuyoCulculateService;
 
 drop table if exists Kyuyo_details;
-drop table if exists Syain;
 drop table if exists Zangyo;
-drop table if exists Yakusyoku;
+drop table if exists Syain;
 drop table if exists Bumon;
+drop table if exists Yakusyoku;
 
+
+create table Yakusyoku(
+    id INT UNSIGNED,
+    name VARCHAR(50),
+    teate INT,
+    PRIMARY KEY (id)
+);
+
+create table Bumon(
+    id INT UNSIGNED,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+
+create table Syain(
+    id INT UNSIGNED,
+    name VARCHAR(50),
+    bumonId INT UNSIGNED,
+    yakusyokuId INT UNSIGNED,
+    kihonkyu INT,
+    nyusyabi DATE,
+    taisyabi DATE,
+    updatedAt DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (bumonId) REFERENCES Bumon(id),
+    FOREIGN KEY (yakusyokuId) REFERENCES Yakusyoku(id)
+);
+
+create table Zangyo(
+    id INT UNSIGNED,
+    tuzyo INT,
+    kyuzitu INT,
+    sinya INT,
+    syainId INT UNSIGNED,
+    nengetu DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (syainId) REFERENCES Syain(id)
+);
 
 create table Kyuyo_details(
     id INT UNSIGNED,
@@ -22,56 +60,26 @@ create table Kyuyo_details(
     siharaibi DATE,
     createdAt DATE,
     updatedAt DATE,
-    deletedAt DATE
+    deletedAt DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (zangyoId) REFERENCES Zangyo(id),
+    FOREIGN KEY (syainId) REFERENCES Syain(id)
 );
 
-create table Syain(
-    id INT UNSIGNED,
-    name VARCHAR(50),
-    bumonId INT UNSIGNED,
-    yakusyokuId INT UNSIGNED,
-    kihonkyu INT,
-    nyusyabi DATE,
-    taisyabi DATE,
-    updatedAt DATE
-);
 
-create table Zangyo(
-    id INT UNSIGNED,
-    tuzyo INT,
-    kyuzitu INT,
-    sinya INT,
-    syainId INT,
-    nengetu DATE
-);
 
-create table Yakusyoku(
-    id INT UNSIGNED,
-    name VARCHAR(50),
-    teate INT
-);
-
-create table Bumon(
-    id INT UNSIGNED,
-    name VARCHAR(50)
-);
-
-INSERT INTO Kyuyo_details(id,kihonkyu,yakusyokuTeate,tukinTeate,zangyoId,zyuminzei,syakaihoken,syotokuzei,syainId,siharaibi,createdAt,updatedAt,deletedAt)
+INSERT INTO Yakusyoku(id, name, teate)
 VALUES
-(1, 250000, 50000, 20000, 1, 10000, 30000, 20000, 1, '2022-04-01', NOW(), NOW(), NULL),
-(2, 200000, 40000, 15000, 2, 8000, 25000, 15000, 2, '2022-04-01', NOW(), NOW(), NULL),
-(3, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-04-01', NOW(), NOW(), NULL),
-(4, 300000, 60000, 25000, 3, 15000, 40000, 25000, 4, '2022-04-01', NOW(), NOW(), NULL),
-(5, 250000, 50000, 20000, 1, 10000, 30000, 20000, 1, '2022-05-01', NOW(), NOW(), NULL),
-(6, 200000, 40000, 15000, 2, 8000, 25000, 15000, 2, '2022-05-01', NOW(), NOW(), NULL),
-(7, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-05-01', NOW(), NOW(), NULL),
-(8, 300000, 60000, 25000, 3, 15000, 40000, 25000, 4, '2022-05-01', NOW(), NOW(), NULL),
-(9, 250000, 50000, 20000, 1, 10000, 30000, 20000, 1, '2022-06-01', NOW(), NOW(), NULL),
-(10, 200000, 40000, 15000, 2, 8000, 25000, 15000, 2, '2022-06-01', NOW(), NOW(), NULL),
-(11, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-06-01', NOW(), NOW(), NULL),
-(12, 300000, 60000, 25000, 3, 15000, 40000, 25000, 4, '2022-06-01', NOW(), NOW(), NULL),
-(13, 250000, 50000, 20000, 1, 10000, 30000, 20000, 1, '2022-07-01', NOW(), NOW(), NULL),
-(14, 200000, 40000, 15000, 2, 8000, 25000, 15000, 2, '2022-07-01', NOW(), NOW(), NULL);
+(1, '部長', 50000),
+(2, '課長', 40000),
+(3, '主任', 30000),
+(4, '一般', 0);
+
+INSERT INTO Bumon(id, name)
+VALUES
+(1, '営業部'),
+(2, 'システム部'),
+(3, '総務部');
 
 INSERT INTO Syain(id,name,bumonId,yakusyokuId,kihonkyu,nyusyabi,taisyabi,updatedAt)
 VALUES
@@ -92,17 +100,26 @@ VALUES
 (1, 2, 4, 8, 1, '2022-03-01'),
 (2, 3, 6, 12, 2, '2022-03-01'),
 (3, 1, 2, 4, 4, '2022-03-01'),
-(4, 4, 8, 16, 1, '2022-02-01');
+(4, 2, 4, 8, 1, '2022-03-01'),
+(5, 3, 6, 12, 2, '2022-03-01'),
+(6, 1, 2, 4, 4, '2022-03-01'),
+(7, 3, 6, 12, 2, '2022-03-01'),
+(8, 1, 2, 4, 4, '2022-03-01'),
+(9, 4, 8, 16, 1, '2022-02-01');
 
-INSERT INTO Yakusyoku(id, name, teate)
+INSERT INTO Kyuyo_details(id,kihonkyu,yakusyokuTeate,tukinTeate,zangyoId,zyuminzei,syakaihoken,syotokuzei,syainId,siharaibi,createdAt,updatedAt,deletedAt)
 VALUES
-(1, '部長', 50000),
-(2, '課長', 40000),
-(3, '主任', 30000),
-(4, '一般', 0);
-
-INSERT INTO Bumon(id, name)
-VALUES
-(1, '営業部'),
-(2, 'システム部'),
-(3, '総務部');
+(1, 250000, 50000, 20000, 1, 10000, 30000, 20000, 1, '2022-04-01', NOW(), NOW(), NULL),
+(2, 200000, 40000, 15000, 2, 8000, 25000, 15000, 2, '2022-04-01', NOW(), NOW(), NULL),
+(3, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-04-01', NOW(), NOW(), NULL),
+(4, 300000, 60000, 25000, 3, 15000, 40000, 25000, 4, '2022-04-01', NOW(), NOW(), NULL),
+(5, 250000, 50000, 20000, 4, 10000, 30000, 20000, 1, '2022-05-01', NOW(), NOW(), NULL),
+(6, 200000, 40000, 15000, 5, 8000, 25000, 15000, 2, '2022-05-01', NOW(), NOW(), NULL),
+(7, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-05-01', NOW(), NOW(), NULL),
+(8, 300000, 60000, 25000, 6, 15000, 40000, 25000, 4, '2022-05-01', NOW(), NOW(), NULL),
+(9, 250000, 50000, 20000, 7, 10000, 30000, 20000, 1, '2022-06-01', NOW(), NOW(), NULL),
+(10, 200000, 40000, 15000, 8, 8000, 25000, 15000, 2, '2022-06-01', NOW(), NOW(), NULL),
+(11, 180000, 30000, 12000, NULL, 7000, 20000, 12000, 3, '2022-06-01', NOW(), NOW(), NULL),
+(12, 300000, 60000, 25000, 9, 15000, 40000, 25000, 4, '2022-06-01', NOW(), NOW(), NULL),
+(13, 250000, 50000, 20000, NULL, 10000, 30000, 20000, 1, '2022-07-01', NOW(), NOW(), NULL),
+(14, 200000, 40000, 15000, NULL, 8000, 25000, 15000, 2, '2022-07-01', NOW(), NOW(), NULL);
