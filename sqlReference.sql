0761f280bec9c7a9d8b46f44477f9ff9593d7e95
seicham <onigirimentai422@gmail.com>
2023-04-11 18:24:37 +0900

    集計関数まわり

--- sqlReference.sql
+++ sqlReference.sql
@@ -79,3 +79,54 @@ END;
 
 -- 和集合
 -- UNIONは重複する行は一つにまとめる。UNION ALLはそのまま。
+-- 型が一緒なら結合することができる。
+
+select * from newstudents
+UNION
+select * from students
+order by id;
+
+select * from newstudents
+union ALL
+select * from students
+order by id;
+
+select id,name from users
+union
+select age,name from newusers
+order by id;
+
+-- 集計関数
+-- nullも含めてカウントされる。
+select count(*) from customers;
+-- nullはカウントされない
+select count(name) from customers; 
+select max(age),min(age) from users where birth _place="日本";
+select max(birth_day),min(birth_day) from users;
+
+select sum(salary) from from employees;
+
+
+-- グループに分ける場合
+
+select age,count(*),max(birth_day),min(birth_day) from users
+WHERE birth_place ="日本"
+group by age
+order by count(*);
+
+select department,sum(salary),floor(avg(salary)),min(salary) from employees
+group by department;
+
+
+select case
+        when birth_place ="日本" then "日本人"
+        else "その他"
+    end,
+    count(*),
+    max(age) 
+    from users
+group by 
+    case
+        when birth_place ="日本" then "日本人"
+        else "その他"
+    end;
